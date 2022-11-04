289c9aedc1073d8ad435e15cef9a0f127fea3164
eggzdee <victorli.freddy@gmail.com>
2022-11-04 00:09:11 -0700

    Health and hunger bars and walking/running

--- Assets/Scripts/BarBehavior.cs
+++ Assets/Scripts/BarBehavior.cs
@@ -5,32 +5,53 @@ using UnityEngine.UI;
 
 public class BarBehavior : MonoBehaviour
 {
-    public int hunger;
-    public int health;
-    private float healthTime;
+    public float hunger;
+    public float health;
+    public float healthRegenPerSecond;
+    public float hungerDegenPerSecondStill;
+    public float hungerDegenPerSecondWalking;
+    public float hungerDegenPerSecondRunning;
     [SerializeField]
-    private Image healthSprite;
+    private Image healthSprite, hungerSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        updateHealth();
+        updateHealthBar();
+        updateHungerBar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthTime += Time.deltaTime;
-        if (hunger != 0 && healthTime >= 100/hunger)
+        health += healthRegenPerSecond * Time.deltaTime * hunger/100;
+        updateHealthBar();
+        
+        switch (GetComponent<PlayerMovement>().moveState)
         {
-            health++;
-            healthTime = 0;
-            updateHealth();
+            case "Still":
+                hunger -= hungerDegenPerSecondStill * Time.deltaTime;
+                break;
+            case "Walking":
+                hunger -= hungerDegenPerSecondWalking * Time.deltaTime;
+                break;
+            case "Running":
+                hunger -= hungerDegenPerSecondRunning * Time.deltaTime;
+                break;
         }
+        if (hunger < 0) hunger = 0;
+        updateHungerBar();
     }
 
-    public void updateHealth()
+    public void updateHealthBar()
     {
-        healthSprite.fillAmount = health/100;
+        Debug.Log("Entered updateHealthBar");
+        healthSprite.fillAmount = health/100f;
+    }
+
+    public void updateHungerBar()
+    {
+        Debug.Log("Entered updateHungerBar");
+        hungerSprite.fillAmount = hunger/100f;
     }
 }
